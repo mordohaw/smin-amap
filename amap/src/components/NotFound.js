ba150cca742d6b9221b802b7a522426e9d038803
mordohaw <william.mordohay@gmail.com>
2017-12-13 13:54:07 +0100

    manage the url

---
+++ amap/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NotFound extends React.Component{
+	render(){
+		return (
+			<h2>404 not found</h2>			
+			)
+
+	}
+}
+
+export default NotFound;
--- amap/src/index.js
+++ amap/src/index.js
@@ -4,7 +4,20 @@ import React from 'react';
 import { render } from 'react-dom';
 //own component import
 import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 import './css/style.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
+if (window.location.pathname == "/store") {
+	render(<App/>, document.querySelector('#main'));
+}else if(window.location.pathname == "/"){
+	render(<StorePicker/>, document.querySelector('#main'));
+}else{
+	render(<NotFound/>, document.querySelector('#main'));
+}
 
-render(<App/>, document.querySelector('#main'));
